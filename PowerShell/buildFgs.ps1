c52c14459eab1bfe381464e64d7a8b5ed697a0e6
阳光 叶 <1113735318@qq.com>
2026-08-20 11:45:04 +0800

    add

---
+++ PowerShell/buildFgs.ps1
@@ -0,0 +1,59 @@
+﻿param(
+  [string]$configuration = "Debug"
+)
+
+$scriptPath = $PSScriptRoot
+if ( $configuration -eq "Debug" ) {
+  $buildPath = [System.IO.Path]::Combine($scriptPath, "build", "Debug", "net10.0");
+  $backupPath = [System.IO.Path]::Combine($scriptPath, "build", "Debug", "temp_net10.0");
+}
+elseif ( $configuration -eq "Release" ) {
+  $buildPath = [System.IO.Path]::Combine($scriptPath, "build", "Release", "net10.0");
+  $backupPath = [System.IO.Path]::Combine($scriptPath, "build", "Release", "temp_net10.0");
+}
+else {
+  Write-Host 未知参数 $configuration
+  return
+}
+
+$desktopPath = [Environment]::GetFolderPath('Desktop')
+$slnPath = Join-Path $scriptPath "zocono.fgs.host.slnx"
+$zipPath = Join-Path $desktopPath "host.zip"
+
+function Ren-Back {
+  if (Test-Path $buildPath) {
+    rm -Path $buildPath -Recurse
+  }	  
+  if (Test-Path $backupPath) {
+    ren $backupPath $buildPath
+  }
+} 
+
+if (!(Test-Path $slnPath)) {
+  Write-Host ${slnPath}解决方案不存在
+  Read-Host -Prompt "按 Enter 键继续..."
+  return
+}
+
+if (Test-Path $buildPath) {
+  ren $buildPath $backupPath
+  if (!$?) {
+    Write-Host 备份当前的目录，重命名文件夹失败
+	Read-Host -Prompt "按 Enter 键继续..."
+    return
+  }
+}
+try {
+  dotnet clean $slnPath -c $configuration
+  dotnet build $slnPath -c $configuration -v d
+  if ($LASTEXITCODE -ne 0) {
+    Write-Host 生成解决方案失败
+    return
+  }
+  Compress-Archive -Path $buildPath -DestinationPath $zipPath -Force
+}
+finally {
+  Ren-Back
+}
+Read-Host -Prompt "按 Enter 键继续..."
+return
---
+++ PowerShell/select.ps1
@@ -0,0 +1,56 @@
+﻿$scriptPath = "D:\origin\zocono.fgs.vnext.host"
+$desktopPath = [Environment]::GetFolderPath('Desktop')
+$slnPath = Join-Path $scriptPath "zocono.fgs.host.slnx"
+$buildPath = [System.IO.Path]::Combine($scriptPath, "build", "Debug", "net10.0");
+$backupPath = [System.IO.Path]::Combine($scriptPath, "build", "Debug", "temp_net10.0");
+$hostZipPath = Join-Path $desktopPath "host.zip"
+
+function Select-One {
+  param(
+    $Title,
+	[Parameter(Mandatory,ValueFromPipeline)]
+	[string[]]$Items
+  )
+  Write-Host $Title
+  $x0 = [Console]::CursorLeft
+  $y0 = [Console]::CursorTop
+  
+  $count = $Items.Length
+  $cur = 0;
+  do {
+	  [Console]::SetCursorPosition($x0,$y0)
+	  
+	  for ($i = 0; $i -lt $count; $i++){
+		if($cur -eq $i) { 
+	      Write-Host " -> $($Items[$i])`n" -ForegroundColor Yellow -NoNewline
+		}
+		else {
+		  Write-Host "    $($Items[$i])`n" -NoNewline
+	    }
+	  }
+	  
+    $key = [Console]::ReadKey($true) 
+	switch ($key.Key){
+	  UpArrow {
+	    $cur--
+		if ( $cur -lt 0 ) { $cur = 0 }
+	  }
+	  DownArrow {
+	    $cur++
+		if ( $cur -ge $count ) { $cur = $count - 1 }
+	  }
+	  Enter {
+		return $cur
+	  }
+	  Escape {
+		return -1
+	  }
+	}
+  }while ($true)
+}
+try{
+$result = Select-One -Title 请选择请选择请选择请选择请选择请选择请选择 请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择1, 请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择2,      请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择请选择3
+Write-Host 你选择了$result
+}catch {
+	Write-Host "出错了" -Foreground Red
+}
